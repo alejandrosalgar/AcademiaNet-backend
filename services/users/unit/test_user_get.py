04535f8ffa2c162165c7a92f69cc1049e0583cf9
Carlos Bolivar <carlos.bolivar@neostella.com>
2024-09-10 22:13:55 -0500

    add unit test using db mocking

--- services/users/unit/test_user_get.py
+++ services/users/unit/test_user_get.py
@@ -1,9 +1,16 @@
+# flake8: noqa
+from core_utils.constants import UserAgent
+from core_utils.http_utils.enums import StatusCodes, UserAgents
+from core_utils.sql_handler.sql_manager import SqlContext
+
+from utils.sql_handler.pg_client import PGClient
+
+SqlContext.DEFAULT_SQL_MANAGER = PGClient()
+UserAgent.DEFAULT_USER_AGENT = UserAgents.APP_KEY_USER
+
 from uuid import uuid4
 
 import src
-from core_utils.auth_utils.permissions import Permission
-from core_utils.core_models.users import Users
-from core_utils.http_utils.enums import StatusCodes
 from core_utils.lambda_utils.models.events import ApiGatewayEvent
 from core_utils.lambda_utils.models.exceptions import ApiGatewayEventException
 from core_utils.lambda_utils.models.responses import ApiGawewayEventResponse
@@ -12,21 +19,20 @@ from custom_logger import _get_default_logger_configuration
 from pytest import raises
 from pytest_mock import MockerFixture
 from src.services.user_get import perform
+
 from utils.mocks import LambdaContext, mock_decorator
 
 mock_decorator("lambda_wrapper")
+from src.handlers.user_get import lambda_handler
 
-from src.handlers.user_get import lambda_handler  # isort:skip # noqa: E402
+_get_default_logger_configuration().register_white_list_fields(["traceback", "sql"])
+USER_ID = "845d59af-036f-48fc-8f1a-26630a7af8e0"
 
-_get_default_logger_configuration().register_white_list_fields(["traceback"])
 
-
-def test_user_get(mocker: MockerFixture):
-    event = ApiGatewayEvent(http_event_mock())
-    mocker.patch("src.handlers.user_get.check_api_keys", return_value=(None, None))
-    mocker.patch("src.handlers.user_get.select_tenant_database", return_value=None)
-    mocker.patch.object(Permission, "check_permissions", return_value=lambda: None)
-    mocker.patch("src.handlers.user_get.perform", return_value=Users())
+def test_user_get():
+    event = ApiGatewayEvent(
+        http_event_mock(user_id=USER_ID, path_parameters={"cognito_user_id": USER_ID})
+    )
 
     result: ApiGawewayEventResponse = lambda_handler(event, LambdaContext())
     assert result.status_code == StatusCodes.OK
@@ -40,20 +46,13 @@ def test_user_get_perform_uuid_except(mocker: MockerFixture):
     assert result.value.status_code == StatusCodes.BAD_REQUEST
 
 
-def test_user_get_not_found(mocker: MockerFixture):
-    mocker.patch("src.services.user_get.get_details", return_value=None)
+def test_user_get_not_found():
     with raises(ApiGatewayEventException) as result:
         perform(uuid4().__str__())
-
-    assert src.services.user_get.get_details.call_count == 1
     assert result.value.status_code == StatusCodes.NOT_FOUND
 
 
-def test_user_get_success(mocker: MockerFixture):
-    mocker.patch("src.services.user_get.validate_uuid", return_value=True)
-    mocker.patch("src.services.user_get.get_details", return_value=Users())
-    user = perform(uuid4().__str__())
+def test_user_get_success():
+    user = perform(USER_ID)
 
-    assert src.services.user_get.validate_uuid.call_count == 1
-    assert src.services.user_get.get_details.call_count == 1
     assert hasattr(user, "cognito_user_id")
--- utils/sql_handler/sql_execution.py
+++ utils/sql_handler/sql_execution.py
@@ -1,10 +1,3 @@
-from core_utils.boto3_utils.rds import RdsClient
 from core_utils.sql_handler.sql_manager import SqlContext
 
-
-def create_sql_context(rds_client):
-    return SqlContext(rds_client)
-
-
-rds_client_instance = RdsClient()
-sql_context = create_sql_context(rds_client_instance)
+sql_context = SqlContext(SqlContext.DEFAULT_SQL_MANAGER)
